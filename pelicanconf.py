bb3956dd2083700c9c9515ef0aac7166b34530b0
Alexander Todorov <atodorov@redhat.com>
2015-11-25 13:55:33 +0200

    Add nice header image

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,7 +16,7 @@ TAG_SAVE_AS = TAG_URL + 'index.html'
 # static paths will be copied under the same name
 STATIC_PATHS = ["images/", 'robots.txt', 'favicon.png', 'CNAME', 'override.css']
 
-HEADER_COVER = "/images/bricks.jpg"
+HEADER_COVER = "/images/header_02.jpg"
 
 COLOR_SCHEME_CSS = "github.css"
 
@@ -34,11 +34,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 20
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
