d62663f5aa5ba006c05f48ac04989b79fb1691cd
Alexander Todorov <atodorov@redhat.com>
2016-01-17 16:24:34 +0200

    update to newest theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -92,5 +92,12 @@ TWITTER_URL = "https://twitter.com/%s" % TWITTER_USERNAME
 GITHUB_URL = "https://github.com/atodorov"
 LINKEDIN_URL = "https://bg.linkedin.com/in/alextodorov"
 
+SOCIAL = (
+            ('twitter',  TWITTER_URL),
+            ('github',   GITHUB_URL),
+            ('linkedin', LINKEDIN_URL),
+        )
+
+
 ADDTHIS_PUBID = "ra-5103cc5a2bc6ba17"
 FEEDBURNER_URL = "http://feeds.feedburner.com/atodorov"
