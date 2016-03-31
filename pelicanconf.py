5cc376ca8b49f35ca617c9850212b31a5b12367e
Alexander Todorov <atodorov@redhat.com>
2016-03-31 13:25:06 +0300

    Get rid of extra config variables

--- pelicanconf.py
+++ pelicanconf.py
@@ -95,17 +95,11 @@ DEFAULT_PAGINATION = 20
 
 THEME = '../pelican-clean-blog'
 
-TWITTER_USERNAME = "atodorov_"
-TWITTER_URL = "https://twitter.com/%s" % TWITTER_USERNAME
-GITHUB_URL = "https://github.com/atodorov"
-LINKEDIN_URL = "https://bg.linkedin.com/in/alextodorov"
-
 ADDTHIS_PUBID = "ra-5103cc5a2bc6ba17"
-FEEDBURNER_URL = "http://feeds.feedburner.com/atodorov"
 
 SOCIAL = (
-            ('twitter',  TWITTER_URL),
-            ('github',   GITHUB_URL),
-            ('linkedin', LINKEDIN_URL),
-            ('rss', FEEDBURNER_URL),
+            ('twitter',  "https://twitter.com/atodorov_"),
+            ('github',   "https://github.com/atodorov"),
+            ('linkedin', "https://bg.linkedin.com/in/alextodorov"),
+            ('rss',      "http://feeds.feedburner.com/atodorov"),
         )
